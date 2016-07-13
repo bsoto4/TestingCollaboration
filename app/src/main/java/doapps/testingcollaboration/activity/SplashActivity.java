353df3d0c8a20345c555a680fe5601a96ec0a18c
Luis Alberto <torresluis082@gmail.com>
2016-07-13 15:37:27 -0500

    acitivities

--- app/src/main/java/doapps/testingcollaboration/activity/SplashActivity.java
+++ app/src/main/java/doapps/testingcollaboration/activity/SplashActivity.java
@@ -2,22 +2,15 @@ package doapps.testingcollaboration.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 
 import doapps.testingcollaboration.R;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        toolbar.setTitle("");
-
-        setSupportActionBar(toolbar);
-
+        setContentView(R.layout.activity_splash2);
 
     }
 }
