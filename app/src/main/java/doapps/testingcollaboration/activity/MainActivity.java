a63ee0d00c56cb8c63597665872c9ff0584e69a7
Luis Alberto <torresluis082@gmail.com>
2016-07-13 20:16:21 -0500

    tabhost

--- app/src/main/java/doapps/testingcollaboration/activity/MainActivity.java
+++ app/src/main/java/doapps/testingcollaboration/activity/MainActivity.java
@@ -1,13 +1,20 @@
 package doapps.testingcollaboration.activity;
 
+import android.content.res.Resources;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.TabHost;
+import android.widget.TextView;
 
 import doapps.testingcollaboration.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    final int color = 0xFFFF0000;
+    final Drawable drawable = new ColorDrawable(color);
+    private TabHost tabs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,9 +22,52 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
         toolbar.setTitle("");
-
         setSupportActionBar(toolbar);
+        init();
+    }
 
+    private void init() {
+        Resources res = getResources();
 
+        tabs = (TabHost) findViewById(android.R.id.tabhost);
+        tabs.setup();
+
+        TabHost.TabSpec spec = tabs.newTabSpec("mitab1");
+
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("PORTADA", res.getDrawable(R.drawable.launcher));
+
+        tabs.addTab(spec);
+
+        spec = tabs.newTabSpec("mitab2");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("DEPORTES", getResources().getDrawable(android.R.drawable.ic_dialog_map));
+
+        tabs.addTab(spec);
+
+        spec = tabs.newTabSpec("mitab3");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("GASTRONOMIA", getResources().getDrawable(android.R.drawable.ic_dialog_map));
+
+        tabs.addTab(spec);
+
+        spec = tabs.newTabSpec("mitab4");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("ESPECIALES", getResources().getDrawable(android.R.drawable.ic_dialog_map));
+
+        tabs.addTab(spec);
+
+        tabs.setCurrentTab(0);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        for(int i=0;i<tabs.getTabWidget().getChildCount();i++)
+        {
+            TextView tv = (TextView) tabs.getTabWidget().getChildAt(i).findViewById(android.R.id.title);
+            tv.setTextColor(getResources().getColor(R.color.primary_text));
+        }
     }
 }
--- app/src/main/java/doapps/testingcollaboration/thread/ProgressThread.java
+++ app/src/main/java/doapps/testingcollaboration/thread/ProgressThread.java
@@ -1,7 +1,7 @@
 package doapps.testingcollaboration.thread;
 
 import android.app.Activity;
- import android.content.Context;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.util.Log;
@@ -19,54 +19,51 @@ import doapps.testingcollaboration.activity.SplashActivity;
 public class ProgressThread extends Thread {
     private static final String TAG = "ProgressThread";
     private Context context;
-    private int i=0;
-    private ProgressBar pg ;
+    private int i = 0;
+    private ProgressBar pg;
     private Activity act;
     private TextView tv;
     private Handler handler;
 
     public ProgressThread(Context context) {
         this.context = context;
-        act = ((Activity)context);
+        act = ((Activity) context);
         //pg =  (((ProgressBar)((Activity)context).findViewById(R.id.progressBar)));
-        pg =  (((ProgressBar)act.findViewById(R.id.progressBar)));
-        tv =  (((TextView) act.findViewById(R.id.textView)));
+        pg = (((ProgressBar) act.findViewById(R.id.progressBar)));
+        tv = (((TextView) act.findViewById(R.id.textView)));
         handler = new Handler();
 
     }
 
 
-
     @Override
     public void run() {
-        while(i<101){
-            Log.e(TAG,""+i);
-            try{
+        while (i < 101) {
+            Log.e(TAG, "" + i);
+            try {
                 act.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                       // (((ProgressBar)((Activity)context).findViewById(R.id.progressBar))).setProgress(i);
+                        // (((ProgressBar)((Activity)context).findViewById(R.id.progressBar))).setProgress(i);
                         pg.setProgress(i);
-                        tv.setText("Cargando ... "+i+"%");
+                        tv.setText("Cargando ... " + i + "%");
                     }
                 });
                 Thread.sleep(50);
-            }
-            catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-            if(i==100){
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        Intent intent = new Intent(act, MainActivity.class);
-                        act.startActivity(intent);
-                        act.finish();
-                    }
-                }, 3000);
-
+            if (i == 100) {
+                //  handler.postDelayed(new Runnable() {
+                //      @Override
+                //      public void run() {
+                Intent intent = new Intent(act, MainActivity.class);
+                act.startActivity(intent);
+                act.finish();
+                //     }
+                //  }, 3000);
+                break;
             }
-
             i++;
         }
     }
